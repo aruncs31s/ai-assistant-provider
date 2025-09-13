2bff5d2d6d93b60f95cd6c62ab15da061fa2a598
Arun cs <aruncs31ss@gmail.com>
2025-09-13 10:35:40 +0530

    update README.md

--- ai_assistant_provider/__init__.py
+++ ai_assistant_provider/__init__.py
@@ -1,1 +1,4 @@
-from .ai_assistant_provider import AiProvider
+from .ai_assistant_provider import AiProvider, AiProviderStatus, AiProviderList
+
+__version__ = "0.0.3"
+__all__ = ["AiProvider", "AiProviderStatus", "AiProviderList"]
